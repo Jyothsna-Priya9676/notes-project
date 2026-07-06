43e4436e2fe3e916e82bc971c5ce8926f916d343
Jyothsna-Priya9676 <ardhamalajyothsnapriya@gmail.com>
2026-07-06 17:24:57 +0530

    Enable note box creation on container click
    Added functionality to open note box on click in notes container.

--- notesproject.js
+++ notesproject.js
@@ -1,49 +1,39 @@
-const notescontainer=document.querySelector(".notes-cotainer");
-const createbtn=document.querySelector(".btn");
-let notes=document.querySelector(".input-box");
-
+const notescontainer=document.querySelector(".notes-cotainer")
+const createbtn=document.querySelector(".btn")
+let notes=document.querySelector(".input-box")
 function showNotes(){
-    notescontainer.innerHTML=localStorage.getItem("notes");
+    notescontainer.innerHTML=localStorage.getItem("notes")
 }
-
 function updateStorage(){
-    localStorage.setItem("notes",notescontainer.innerHTML);
+    localStorage.setItem("notes",notescontainer.innerHTML)
 }
-
-showNotes();
-
+showNotes()
 createbtn.addEventListener("click",()=>{
-    let inputbox=document.createElement("p");
-    let img=document.createElement("img");
-
-    inputbox.className="input-box";
-    inputbox.setAttribute("contenteditable","true");
-
-    img.src="https://cdn-icons-png.flaticon.com/128/2438/2438317.png";
-
-    notescontainer.appendChild(inputbox).appendChild(img);
-
-    inputbox.focus();
-});
-
+    let inputbox=document.createElement("p")
+    let img=document.createElement("img")
+    inputbox.className="input-box"
+    inputbox.setAttribute("contenteditable","true")
+    img.src="https://cdn-icons-png.flaticon.com/128/2438/2438317.png"
+    notescontainer.appendChild(inputbox).appendChild(img)
+})
 notescontainer.addEventListener("click",function(e){
     if(e.target.tagName==="IMG"){
-        e.target.parentElement.remove();
-        updateStorage();
+        e.target.parentElement.remove()
+        updateStorage()
     }
     else if(e.target.tagName==="P"){
-        notes=document.querySelectorAll(".input-box");
-        notes.forEach(nt=>{
+        notes=document.querySelectorAll(".input-box")
+        notes.forEach(nt=> {
             nt.onkeyup=function(){
-                updateStorage();
+                updateStorage()
             }
+            
         });
     }
-});
-
+})
 document.addEventListener("keydown",event=>{
     if(event.key==="Enter"){
-        document.execCommand("insertLineBreak");
-        event.preventDefault();
+        document.execCommand("insertLineBreak")
+        event.preventDefault()
     }
-});
+})   i wnat when we click whereever in that box in create notes note box open 
