e72da85a5dce148bc5fe4f910b11711f666ad322
Jyothsna-Priya9676 <ardhamalajyothsnapriya@gmail.com>
2026-07-06 17:30:15 +0530

    Update notesproject.js

--- notesproject.js
+++ notesproject.js
@@ -1,39 +1,48 @@
-const notescontainer=document.querySelector(".notes-cotainer")
-const createbtn=document.querySelector(".btn")
-let notes=document.querySelector(".input-box")
-function showNotes(){
-    notescontainer.innerHTML=localStorage.getItem("notes")
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+
+function updateStorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
 }
-function updateStorage(){
-    localStorage.setItem("notes",notescontainer.innerHTML)
+
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes") || "";
 }
-showNotes()
-createbtn.addEventListener("click",()=>{
-    let inputbox=document.createElement("p")
-    let img=document.createElement("img")
-    inputbox.className="input-box"
-    inputbox.setAttribute("contenteditable","true")
-    img.src="https://cdn-icons-png.flaticon.com/128/2438/2438317.png"
-    notescontainer.appendChild(inputbox).appendChild(img)
-})
-notescontainer.addEventListener("click",function(e){
-    if(e.target.tagName==="IMG"){
-        e.target.parentElement.remove()
-        updateStorage()
+
+showNotes();
+
+function createNote() {
+    let note = document.createElement("p");
+    let img = document.createElement("img");
+
+    note.className = "input-box";
+    note.setAttribute("contenteditable", "true");
+
+    img.src = "https://cdn-icons-png.flaticon.com/128/2438/2438317.png";
+
+    note.appendChild(img);
+    notesContainer.appendChild(note);
+
+    updateStorage();
+}
+createBtn.addEventListener("click", createNote);
+notesContainer.addEventListener("click", function (e) {
+    if (e.target === notesContainer) {
+        createNote();
     }
-    else if(e.target.tagName==="P"){
-        notes=document.querySelectorAll(".input-box")
-        notes.forEach(nt=> {
-            nt.onkeyup=function(){
-                updateStorage()
-            }
-            
-        });
+});
+notesContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+        updateStorage();
     }
-})
-document.addEventListener("keydown",event=>{
-    if(event.key==="Enter"){
-        document.execCommand("insertLineBreak")
-        event.preventDefault()
+});
+notesContainer.addEventListener("keyup", function () {
+    updateStorage();
+});
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
     }
-})   i wnat when we click whereever in that box in create notes note box open 
+});
