44c7316155a1577d4d3595505d9f018adc581fa5
Jyothsna-Priya9676 <ardhamalajyothsnapriya@gmail.com>
2026-07-06 17:18:27 +0530

    Update notesproject.js

--- notesproject.js
+++ notesproject.js
@@ -1,4 +1,22 @@
+const notescontainer = document.querySelector(".notes-cotainer");
+const createbtn = document.querySelector(".btn");
+let notes = document.querySelector(".input-box");
+
+function showNotes() {
+    notescontainer.innerHTML = localStorage.getItem("notes");
+}
+
+function updateStorage() {
+    localStorage.setItem("notes", notescontainer.innerHTML);
+}
+
+showNotes();
+
 createbtn.addEventListener("click", () => {
+
+    let note = document.createElement("div");
+    note.className = "note";
+
     let inputbox = document.createElement("p");
     let img = document.createElement("img");
 
@@ -7,20 +25,45 @@ createbtn.addEventListener("click", () => {
 
     img.src = "https://cdn-icons-png.flaticon.com/128/2438/2438317.png";
 
-    notescontainer.appendChild(inputbox);
-    inputbox.appendChild(img);
+    note.appendChild(inputbox);
+    note.appendChild(img);
 
-    // Place cursor inside the note
-    const range = document.createRange();
-    const selection = window.getSelection();
-
-    range.selectNodeContents(inputbox);
-    range.collapse(true);
-
-    selection.removeAllRanges();
-    selection.addRange(range);
+    notescontainer.appendChild(note);
 
     inputbox.focus();
 
     updateStorage();
+
+});
+
+notescontainer.addEventListener("click", function(e){
+
+    if(e.target.tagName==="IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+
+    else if(e.target.classList.contains("input-box")){
+
+        notes=document.querySelectorAll(".input-box");
+
+        notes.forEach(nt=>{
+
+            nt.onkeyup=function(){
+                updateStorage();
+            }
+
+        });
+
+    }
+
+});
+
+document.addEventListener("keydown",event=>{
+
+    if(event.key==="Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+
 });
